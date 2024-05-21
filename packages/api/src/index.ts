3118edc30820790483d702e05ae68c4916b449cb
chadjaaSofiane <sofiane.chadjaa@datamasterai.com>
2024-05-21 17:05:35 +0100

    Create src folder with initial placeholder code.

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,18 @@
+import chalk from "chalk";
+import cors from "cors";
+import express from "express";
 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_, res) => {
+  return res.json({ message: "Hello, World!" });
+});
+
+app.listen(PORT, () => {
+  console.log(chalk.green(`Server listening on port ${PORT}`));
+});
