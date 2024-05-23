cc712d396b74d2aaa1b328b8593f7f46e181b21c
chadjaaSofiane <sofiane.chadjaa@datamasterai.com>
2024-05-23 19:17:10 +0100

    Feat: implement an endpoint for the deletion of a device.

--- packages/api/src/routes/devices/index.ts
+++ packages/api/src/routes/devices/index.ts
@@ -1,16 +1,18 @@
 import validate from "@/middlewares/validate";
 import { Router } from "express";
 import { createDevice, createDeviceSchema } from "./controllers/createDevice";
-import { getDevices, getDevicesSchema } from "./controllers/getDevices";
+import { deleteDevice, deleteDeviceSchema } from "./controllers/deleteDevice";
 import {
   getDeviceById,
   getDeviceByIdSchema,
 } from "./controllers/getDeviceById";
+import { getDevices, getDevicesSchema } from "./controllers/getDevices";
 
 const deviceRouter = Router();
 
 deviceRouter.get("/", validate(getDevicesSchema), getDevices);
 deviceRouter.get("/:id", validate(getDeviceByIdSchema), getDeviceById);
 deviceRouter.post("/", validate(createDeviceSchema), createDevice);
+deviceRouter.delete("/:id", validate(deleteDeviceSchema), deleteDevice);
 
 export default deviceRouter;
