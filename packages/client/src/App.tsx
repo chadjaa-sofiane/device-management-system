2b838e850dcf87d97a47370e8fbc2d6d08c06886
chadjaa-sofiane <sofianne012@gmail.com>
2024-05-24 19:27:33 +0100

    Chore: Install and configure react-router-dom.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,34 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/devices" />,
+  },
+  {
+    path: "/devices",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/devices/create",
+    element: <div>About</div>,
+  },
+  {
+    path: "/devices/update",
+    element: <div>About</div>,
+  },
+  {
+    path: "*",
+    element: <div>Not Found</div>,
+  },
+]);
+
 function App() {
-  return <div className="container min-h-screen m-auto flex flex-col"></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
