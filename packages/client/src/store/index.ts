71c116f22cc82d9c114122c9dfd9e2cf5f47155d
chadjaa-sofiane <sofianne012@gmail.com>
2024-05-26 23:31:22 +0100

    Feat: add middleware to watch the value of page for pagination.

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -1,16 +1,29 @@
-import { configureStore, combineSlices } from "@reduxjs/toolkit";
-import devices from "./devicesSlice";
+import { configureStore, createListenerMiddleware } from "@reduxjs/toolkit";
+import devices, { fetchDevicesAsync } from "./devicesSlice";
+
+const listenerMiddleware = createListenerMiddleware();
 
 export const createStore = () =>
   configureStore({
-    reducer: combineSlices({
+    reducer: {
       devices,
-    }),
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(listenerMiddleware.middleware),
   });
 
 const store = createStore();
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+listenerMiddleware.startListening.withTypes<RootState, AppDispatch>()({
+  predicate: (_, currentState, previousState) => {
+    return currentState.devices.page !== previousState.devices.page;
+  },
+  effect: (_, listenerApi) => {
+    listenerApi.dispatch(fetchDevicesAsync());
+  },
+});
 
 export default store;
