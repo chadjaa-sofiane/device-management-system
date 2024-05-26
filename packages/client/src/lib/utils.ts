3bc7333774f919c79c02014b517bca1d50cce6e7
chadjaa-sofiane <sofianne012@gmail.com>
2024-05-26 19:38:31 +0100

    Feat: create function to map http errors to form.

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { twMerge } from "tailwind-merge";
 import clsx, { ClassValue } from "clsx";
+import type { Path, UseFormSetError } from "react-hook-form";
+import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
@@ -8,4 +9,20 @@ export const cn = (...inputs: ClassValue[]) => {
 export const createSequentialArray = (start: number, end: number, step = 1) => {
   const length = Math.floor((end - start) / step) + 1;
   return Array.from({ length }, (_, i) => start + step * i);
+};
+
+export const mapServerErrorsToForm = <
+  T extends Record<string, string | undefined | null>,
+>(
+  setError: UseFormSetError<Required<T>>,
+  errors: T,
+): void => {
+  if (!errors) return;
+  for (const [key, value] of Object.entries(errors)) {
+    if (!value) return;
+    setError(key as unknown as Path<Required<T>>, {
+      type: "manual",
+      message: value,
+    });
+  }
 };
