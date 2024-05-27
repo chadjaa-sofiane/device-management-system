658b27e8f5f9753036939b098661fff5735265b1
chadjaa-sofiane <sofianne012@gmail.com>
2024-05-27 22:23:55 +0100

    Feat: cerate a function that delete files.

--- packages/api/src/lib/utils.ts
+++ packages/api/src/lib/utils.ts
@@ -1,4 +1,13 @@
+import { unlinkSync } from "fs";
 import { z } from "zod";
 
 export const zodStringToNumber = (schema: z.ZodTypeAny) =>
   z.preprocess((val) => (val ? parseInt(val as string) : undefined), schema);
+
+export const deleteFile = (path: string) => {
+  try {
+    unlinkSync(path);
+  } catch (error) {
+    console.log(error);
+  }
+};
